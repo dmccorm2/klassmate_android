868f4009c934a56892161aad5c51740dde3df3f5
dmccorm2 <dmccorm2@nd.edu>
2014-12-10 01:18:42 -0500

    working upload

--- app/src/main/java/com/example/daniel/myapplication/Home.java
+++ app/src/main/java/com/example/daniel/myapplication/Home.java
@@ -3,22 +3,41 @@ package com.example.daniel.myapplication;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import retrofit.RestAdapter;
+import retrofit.client.Response;
+import retrofit.http.Headers;
+import retrofit.http.Multipart;
+import retrofit.http.POST;
+import retrofit.http.Part;
+import retrofit.mime.TypedFile;
+import retrofit.mime.TypedString;
+
+import static android.webkit.MimeTypeMap.getFileExtensionFromUrl;
+
+
+
 
 public class Home extends Activity {
-
+    private static final int CAPTURE_IMAGE_ACTIVTY_REQUEST_CODE = 100;
+    String image_path = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,29 +47,107 @@ public class Home extends Activity {
         @Override
         public void onClick(View v){
              dispatchTakePictureIntent();
+
+
         }
         });
     }
 
+    public interface FileWebService {
+        @Headers({
+                "Cookie: user_id=2; addCourse=0; courses=j%3A%5B%22CSE40113%22%2C%22CSE40232%22%5D; crns=j%3A%5B11241%2C15",
+                "Referer: http://104.131.82.38:3000/classpage?id=11241",
+
+                })
+        @Multipart
+        @POST("/fileupload")
+        Response upload(@Part("name") TypedFile image,
+                    @Part("filename") TypedString filename,
+                    @Part("title") TypedString title);
+    }
+
+
+
     private void dispatchTakePictureIntent(){
-        final int REQUEST_IMAGE_CAPTURE = 1;
+
+
+
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, CAPTURE_IMAGE_ACTIVTY_REQUEST_CODE);
         if(takePictureIntent.resolveActivity(getPackageManager()) != null){
             File photoFile = null;
             try {
                 photoFile = createImageFile();
+
+
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
             // Continue only if the File was successfully created
             if (photoFile != null) {
+                Log.v("Photo is not null: ", Uri.fromFile(photoFile).toString());
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
                         Uri.fromFile(photoFile));
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+
             }
 
         }
+
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVTY_REQUEST_CODE){
+            if(resultCode == RESULT_OK){
+                // image captured
+                Toast.makeText(this, "Uploading image", Toast.LENGTH_LONG).show();
+                File photo = new File(image_path);
+                new uploadPhoto().execute(photo);
+            }
+            if(resultCode == RESULT_CANCELED){
+                // cancelled image capture
+            }
+            else{
+                Toast.makeText(this, "Failed to take photo", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    class uploadPhoto extends AsyncTask<File, Void, String> {
+
+        private Exception exception;
+
+
+
+        protected void onPostExecute() {
+            // TODO: check this.exception
+            // TODO: do something with the feed
+        }
+
+
+        @Override
+        protected String doInBackground(File... params) {
+            RestAdapter restAdapter = new RestAdapter.Builder()
+                    .setEndpoint("http://104.131.82.38:3000")
+                    .build();
+            URI url = params[0].toURI();
+            String mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(getFileExtensionFromUrl(url.toString()));
+            Log.w("Mimetype: ", mimeType);
+            TypedFile fileToSend;
+            if(params[0].exists()) Log.v("File: ", params[0].getAbsolutePath());
+            FileWebService fws = restAdapter.create(FileWebService.class);
+           // File test = new File("/storage/emulated/0/Pictures/JPEG_20141210_001748_-842374887.jpg");
+            fileToSend = new TypedFile(mimeType, params[0]);
+            TypedString fname = new TypedString("android_test.jpg");
+            fws.upload(fileToSend, fname, fname);
+            return null;
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -72,10 +169,10 @@ public class Home extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
-
+    String mCurrentPhotoPath = "";
     private File createImageFile() throws IOException {
         // Create an image file name
-        String mCurrentPhotoPath = "";
+
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = Environment.getExternalStoragePublicDirectory(
@@ -85,9 +182,12 @@ public class Home extends Activity {
                 ".jpg",         /* suffix */
                 storageDir      /* directory */
         );
-
+        new uploadPhoto().execute(image);
         // Save a file: path for use with ACTION_VIEW intents
         mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        image_path = image.getAbsolutePath();
+        Log.w("image file: ", image_path);
         return image;
     }
+
 }
